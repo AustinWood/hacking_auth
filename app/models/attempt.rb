a4f0aca0bc2d5a5b0886254eaaa8de33f78a86f9
Austin Wood <austin@austinkwood.com>
2017-02-18 15:08:32 -0800

    Annotate models

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: attempts
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  stop_time  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Attempt < ActiveRecord::Base
 end
