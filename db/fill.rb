946139a7d10cd08b2cf88e597101024442cf5d0e
Gilbert <PGilbertSchmitt@gmail.com>
2017-02-18 16:17:43 -0800

    Fix NULL to nil

--- db/fill.rb
+++ db/fill.rb
@@ -15,7 +15,7 @@ raw_questions.each do |question|
   text = question[1][1]
   parent_id = id_hash[question[2][1]]
   if parent_id.nil?
-    string = "Question.create!(text: '#{text}', parent_id: NULL)"
+    string = "Question.create!(text: '#{text}', parent_id: nil)"
   else
     string = "Question.create!(text: '#{text}', parent_id: '#{parent_id}')"
   end
